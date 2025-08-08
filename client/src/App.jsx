786e291ae24a9afcec57f74c512873df99377a57
priyanshu-sahani-10 <priyanshusahani038@gmail.com>
2025-08-08 22:53:50 +0530

    Added feed page feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Routes ,Route} from 'react-router-dom'
+import Layout from "./pages/Layout.jsx"
+import Feed from './pages/Feed.jsx'
+import Connections from './pages/Connections.jsx'
+import Message from './pages/Message.jsx'
+import Discover from './pages/Discover.jsx'
+import Profile from './pages/Profile.jsx'
+import Post from './pages/Post.jsx'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <>
+      <Toaster/>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Feed/>}/>
+          <Route path='messages' element={<Message/>}/>
+          <Route path='connections' element={<Connections/>}/>
+          <Route path='discover' element={<Discover/>}/>
+          <Route path='profile' element={<Profile/>}/>
+          <Route path='create-post' element={<Post/>}/>
+        </Route>
+      </Routes>
+    </>
   )
 }
 
---
+++ client/src/pages/Message.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Message() {
+    return (
+        <>
+        <h1>Messages</h1>
+        </>
+    )
+}
+
+export default Message
